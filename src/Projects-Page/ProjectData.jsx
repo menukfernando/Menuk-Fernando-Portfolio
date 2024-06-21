8d683c40ec5de07671821ac6cc1fb71dd1580a04
menukfernando <100038759+menukfernando@users.noreply.github.com>
2024-06-21 21:10:42 +0530

    update: projects

--- src/Projects-Page/ProjectData.jsx
+++ src/Projects-Page/ProjectData.jsx
@@ -1,39 +1,51 @@
 export const ProjectsData = [
-    {
-      id: 1,
-      name: "Online Food Ordering System",
-      img: "Project1.png",
-      alt: "Project Image",
-      description: "This is an Online Food Ordering System that I built for my Web Development Assignment in Campus.",
-      languages: "PHP, MySQL, JavaScript, HTML5, CSS3",
-      width: "fit-content",
-      iconWidth: "30px",
-      link: "https://github.com/menukfernando/Online_Food_Ordering_System"
-    },
+  {
+    id: 4,
+    name: "My Portfolio",
+    img: "Project4.png",
+    alt: "Project Image",
+    description: "This is my portfolio. I created it using ReactJS.",
+    languages: "HTML5, CSS3, Javascript, ReactJS",
+    width: "fit-content",
+    iconWidth: "30px",
+    link: "https://github.com/menukfernando/Menuk-Fernando-Portfolio",
+  },
+  {
+    id: 1,
+    name: "Online Food Ordering System",
+    img: "Project1.png",
+    alt: "Project Image",
+    description:
+      "This is an Online Food Ordering System that I built for my Web Development Assignment in Campus.",
+    languages: "PHP, MySQL, JavaScript, HTML5, CSS3",
+    width: "fit-content",
+    iconWidth: "30px",
+    link: "https://github.com/menukfernando/Online_Food_Ordering_System",
+  },
 
-    {
-        id: 2,
-        name: "Banking Application",
-        img: "Project2.png",
-        alt: "Project Image",
-        description: "This is a simple console-based banking application implemented in Java. It allows users to perform basic banking operations such as depositing money, withdrawing money, checking balance, and viewing transaction history.",
-        languages: "JAVA",
-        width: "fit-content",
-        iconWidth: "30px",
-        link: "https://github.com/menukfernando/Simple-Banking-Application"
-      },
-      
-      {
-        id: 3,
-        name: "Land Reservation System",
-        img: "Project3.png",
-        alt: "Project Image",
-        description: "This is a simple console-based Land Reservation Application implemented in Java. I built this project for my Programming assignment in campus",
-        languages: "JAVA",
-        width: "fit-content",
-        iconWidth: "30px",
-        link: "https://github.com/menukfernando/Land-Reservation-System/tree/master"
-      }
+  {
+    id: 2,
+    name: "Banking Application",
+    img: "Project2.png",
+    alt: "Project Image",
+    description:
+      "This is a simple console-based banking application implemented in Java. It allows users to perform basic banking operations such as depositing money, withdrawing money, checking balance, and viewing transaction history.",
+    languages: "JAVA",
+    width: "fit-content",
+    iconWidth: "30px",
+    link: "https://github.com/menukfernando/Simple-Banking-Application",
+  },
 
-  ];
-  
+  {
+    id: 3,
+    name: "Land Reservation System",
+    img: "Project3.png",
+    alt: "Project Image",
+    description:
+      "This is a simple console-based Land Reservation Application implemented in Java. I built this project for my Programming assignment in campus",
+    languages: "JAVA",
+    width: "fit-content",
+    iconWidth: "30px",
+    link: "https://github.com/menukfernando/Land-Reservation-System/tree/master",
+  },
+];
