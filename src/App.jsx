ca5cc799a3718e6c2cd3a2322a9dd176ed675f00
menukfernando <100038759+menukfernando@users.noreply.github.com>
2024-07-24 12:13:17 +0530

    under-m

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -24,7 +24,35 @@ function App() {
         <Route path="/blog" element={<Blog BlogData={BlogData} />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/html-html5" element={<Html />} />
-      </Routes>
+      </Routes> */}
+      <div className="container">
+        <div className="content">
+          <h1>Menuk Fernando</h1>
+          <p>Under Maintanence. Please check back soon!</p>
+          <p>
+            Email:{" "}
+            <a href="https://mail.google.com" target="_blank">
+              menukfernando7@gmail.com
+            </a>
+          </p>
+          <p>
+            LinkedIn:
+            <a
+              href="https://www.linkedin.com/in/menuk-fernando"
+              target="_blank"
+            >
+              linkedin.com/menukfernando
+            </a>
+          </p>
+          <p>
+            Instagram:
+            <a href="https://www.instagram.com/menukk___/" target="_blank">
+              menukk___
+            </a>
+          </p>
+          <div className="construction-icon">🚧</div>
+        </div>
+      </div>
     </>
   );
 }
